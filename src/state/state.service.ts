127e0d39106438c0078abdfbf5c1c5ec7c239fdd
HendrywDeyvison <hendryw.deyvison@gmail.com>
2024-03-03 12:10:57 -0300

    feat: create city controller and insert cache

---
+++ src/state/state.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { StateEntity } from './entities/state.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class StateService {
+  constructor(
+    @InjectRepository(StateEntity)
+    private readonly stateRepository: Repository<StateEntity>,
+  ) {}
+
+  async getAllStates(): Promise<StateEntity[]> {
+    return this.stateRepository.find();
+  }
+}
